038843b918fc1bec3dc6cd2406cb99dead2cb2b4
S9037218 <Orangejuice135!>
2021-03-08 14:50:56 +0800

    commit

---
+++ src/com/taozi/MidInter.java
@@ -0,0 +1,13 @@
+package com.taozi;
+
+/*some comments here*/
+
+public interface MidInter {
+
+    ChildInter childInter = null;
+
+    void MidInterTest1(int param1);
+
+    void MidInterTest2(int param2);
+
+}
---
+++ src/com/taozi/ParentInter.java
@@ -0,0 +1,15 @@
+package com.taozi;
+
+/*some comments here*/
+
+public interface ParentInter {
+
+    MidInter midInter = null;
+
+    void parentInterTest1(int param1);
+
+    void parentInterTest2(int param2);
+
+}
+
+
