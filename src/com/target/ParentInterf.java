7c94519c0a7f164d37fe09f81d2ab39614587b2f
S9037218 <Orangejuice135!>
2021-03-08 16:15:49 +0800

    commit

--- src/com/target/ParentInterf.java
+++ src/com/target/ParentInterf.java
@@ -10,7 +10,7 @@ public interface ParentInterf {
 
     void parentInterTest1(int param1);
 
-    void parentInterTest2(int param2);
+    void parentInterTest3(int param2);
 
     ChildClass childClass = null;
 }
